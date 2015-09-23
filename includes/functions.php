61db4ee0a3dcad599ddb537ad9aa2fb06b5b99e7
Dima Timoshenko <tzd0409@gmail.com>
2015-09-23 20:37:26 +0300

    adding files login.php and logfile.php

--- includes/functions.php
+++ includes/functions.php
@@ -37,4 +37,19 @@ function include_layout_template($template="") {
 	include(SITE_ROOT.DS.'public'.DS.'layouts'.DS.$template);
 }
 
+function log_action($action, $message="") {
+    $logfile = SITE_ROOT . DS . 'logs' . DS . 'log.txt';
+    $new = file_exists($logfile) ? false : true ;
+  if( $handle = fopen($logfile, 'a')) {
+    $timestamp = strftime("%Y-%m-%d %H:%M:%S", time());
+    $content ="{$timestamp} | {$action} {$message}\n";
+    fwrite($handle, $content);
+    fclose($handle);
+    if($new) {chmod($logfile, 0766);}
+  } else {
+    echo "Can't create/open logfile.txt ";
+  }
+
+}
+
 ?>
---
+++ public/admin/logfile.php
@@ -0,0 +1,49 @@
+<?php
+require_once("../../includes/initialize.php");
+
+if(!$session->is_logged_in()) {
+    redirect_to("login.php");
+}
+
+$logfile = SITE_ROOT . DS . 'logs' . DS . 'log.txt';
+if($_GET['clear'] == 'true') {
+    file_put_contents($logfile, '');
+    log_action('Log cleared', 'by User Id
+    {$session->user_id}');
+    // redirect to the same page for avoiding
+    // of giving URL logfile.php?clear=true
+    redirect_to('logfile.php');
+}
+
+include_layout_template('admin_header.php');
+?>
+<a href="index.php">&laquo; Back</a><br />
+<br />
+
+<h2>Log File</h2>
+
+
+<a href="logfile.php?clear=true">Clear log file</a>
+
+<?php
+
+if(file_exists($logfile) && is_readable($logfile) &&
+  $handle = fopen($logfile, 'r')) {
+    // откуда взядся class\"log-entries\" ????
+  echo "<ul class\"log-entries\">";
+  while(!feof($handle)) {
+      $entry = fgets($handle);
+      if(trim($entry) != "") {
+          echo "<li>{$entry}</li>";
+      }
+  }
+    echo "</ul>";
+    fclose($handle);
+} else {
+    echo "Could not read from {$logfile}.";
+}
+
+?>
+<?php include_layout_template('admin_footer.php'); ?>
+
+
